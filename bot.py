ad3eb30d89b0bd4f4bbd65d5129f395536fc7d42
corndogit <nathanfinlaybrenton@gmail.com>
2022-10-26 17:46:50 +0100

    update: switched to Disnake library, implemented slash commands!

--- bot.py
+++ bot.py
@@ -2,91 +2,87 @@ import os
 import libs.weather
 
 from libs.doc.weathercodes import weathercodes, decode_uv_index
-import discord
+import disnake
+from disnake.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+jillbot = commands.Bot(
+    command_prefix=disnake.ext.commands.when_mentioned,
+    sync_commands_debug=True
+)
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
 
-
-@client.event
+@jillbot.event
 async def on_ready():
-    print("{0.user} is now online!".format(client))
+    print(f"{jillbot.user} is now online!")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    # Hello World
-    if message.content.startswith('!hello'):
-        await message.reply('Hello there!')
-
-    # About
-    if message.content.startswith('!about'):
-        about_me = discord.Embed(
-            title="About",
-            url="https://github.com/corndogit",
-            description="JillBot is an open-source Discord bot written in Python by corndog#2974"
-        )
-        about_me.set_thumbnail(url="https://avatars.githubusercontent.com/u/101812777?v=4")
-        about_me.add_field(name="Prefix", value="!")
-        about_me.add_field(name="Commands", value="`hello`, `about`, `weather <location>`, `4panel`", inline=False)
-        await message.channel.send(embed=about_me)
-
-    # Uses Discord exploit to make a 4 panel thing from Discord image links
-    if message.content.startswith('!4panel'):
-        link = message.content.split('!4panel ')[1]
-        if not link.startswith('https://'):
-            await message.channel.send('Invalid image link - must start with https://')
-        else:
-            link_content = link.split('https://')[1]
-            await message.channel.send(f'https://{link_content}\n' +
-                                       f'https://\\{link_content}\n' +
-                                       f'https://\\\\{link_content}\n' +
-                                       f'https://\\\\\\{link_content}\n')
-
-    # weather-cli implementation
-    if message.content.startswith('!weather'):
-        command = message.content.split(' ')
-        command.remove('!weather')
-        location = ' '.join(command)
-        await message.channel.send(f"Getting weather for {location}...")
-
-        weather_data = libs.weather.get_weather(location)
-
-        if weather_data == "API_Error":
-            await message.channel.send("API error - your request produced no suggestions.")
-
-        else:
-            embed = discord.Embed(title=f"Showing results for {weather_data['City']}, {weather_data['Country']}",
-                                  url="https://github.com/corndogit/weather-cli/")
-            embed.add_field(name="Type",
-                            value=weathercodes[str(weather_data['SignificantWeatherCode'])],
-                            inline=False)
-            embed.set_thumbnail(url="https://corndog.s-ul.eu/OZbm1tH1.jpg")
-            embed.add_field(name="Max Temperature",
-                            value=f"{round(weather_data['MaxTemperature'])}\u00B0C",
-                            inline=True)
-            embed.add_field(name="Min Temperature",
-                            value=f"{round(weather_data['MinTemperature'])}\u00B0C",
-                            inline=True)
-            embed.add_field(name="Chance of Precipitation",
-                            value=f"{weather_data['ChanceOfPrecipitation']}%",
-                            inline=True)
-            embed.add_field(name="Wind Speed",
-                            value=f"{round(float(weather_data['WindSpeed'] / 0.44704), 1)} mph",
-                            inline=True)
-            embed.add_field(name="Max UV Index Rating",
-                            value=f"{weather_data['MaxUvIndex']} ({decode_uv_index(weather_data['MaxUvIndex'])})",
-                            inline=True)
-            embed.set_footer(text="Data collected from Met Office Weather DataHub")
-            await message.channel.send(embed=embed)
+@jillbot.slash_command(description="Responds with 'Hello there!'")
+async def hello(inter):
+    await inter.response.send_message("Hello there!")
 
 
-client.run(TOKEN)
+@jillbot.slash_command(description="Displays an embed with bot information")
+async def about(inter):
+    about_me = disnake.Embed(
+        title="About",
+        url="https://github.com/corndogit",
+        description="JillBot is an open-source Discord bot written in Python by corndog#2974"
+    )
+    about_me.set_thumbnail(url="https://avatars.githubusercontent.com/u/101812777?v=4")
+    about_me.add_field(name="Prefix", value="!")
+    about_me.add_field(name="Commands", value="`hello`, `about`, `weather <location>`, `4panel`", inline=False)
+    await inter.response.send_message(embed=about_me)
+
+
+@jillbot.slash_command(
+    description="Does a trick with images uploaded to cdn.discordapp.com that makes them display 4 times")
+async def funny(inter, link: str):
+    if not link.startswith('https://'):
+        await inter.response.send_message('Invalid image link - must start with https://')
+    else:
+        link_content = link.split('https://')[1]
+        await inter.response.send_message(f'https://{link_content}\n' +
+                                   f'https://\\{link_content}\n' +
+                                   f'https://\\\\{link_content}\n' +
+                                   f'https://\\\\\\{link_content}\n')
+
+
+@jillbot.slash_command(description="Displays a small weather report for the city provided.")
+async def weather(inter, city: str):
+    # await inter.response.send_message(f"Getting weather for {city}...")
+
+    weather_data = libs.weather.get_weather(city)
+
+    if weather_data == "API_Error":
+        await inter.response.send_message("API error - your request produced no suggestions.")
+
+    else:
+        embed = disnake.Embed(title=f"Showing results for {weather_data['City']}, {weather_data['Country']}",
+                              url="https://github.com/corndogit/weather-cli/")
+        embed.add_field(name="Type",
+                        value=weathercodes[str(weather_data['SignificantWeatherCode'])],
+                        inline=False)
+        embed.set_thumbnail(url="https://corndog.s-ul.eu/OZbm1tH1.jpg")
+        embed.add_field(name="Max Temperature",
+                        value=f"{round(weather_data['MaxTemperature'])}\u00B0C",
+                        inline=True)
+        embed.add_field(name="Min Temperature",
+                        value=f"{round(weather_data['MinTemperature'])}\u00B0C",
+                        inline=True)
+        embed.add_field(name="Chance of Precipitation",
+                        value=f"{weather_data['ChanceOfPrecipitation']}%",
+                        inline=True)
+        embed.add_field(name="Wind Speed",
+                        value=f"{round(float(weather_data['WindSpeed'] / 0.44704), 1)} mph",
+                        inline=True)
+        embed.add_field(name="Max UV Index Rating",
+                        value=f"{weather_data['MaxUvIndex']} ({decode_uv_index(weather_data['MaxUvIndex'])})",
+                        inline=True)
+        embed.set_footer(text="Data collected from Met Office Weather DataHub")
+        await inter.response.send_message(embed=embed)
+
+
+jillbot.run(TOKEN)
